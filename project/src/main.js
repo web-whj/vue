3d4bafedd894c99bdf446ec2260823dfd362a3be
wolf-wh <827240599@qq.com>
2019-02-17 13:33:32 +0800

    2.17 13:33

--- project/src/main.js
+++ project/src/main.js
@@ -3,11 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 import 'css/base.css'
 import 'css/iconfont.css'
 
 Vue.config.productionTip = false
-
+Vue.use(VueAwesomeSwiper)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
---
+++ project/src/pages/Home/components/Swiper.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="imagesBox">
+        <swiper :options="swiperOption">
+            <!-- slides -->
+            <swiper-slide>
+                <img src="@/assets/images/lunbo.jpg" alt="">
+            </swiper-slide>
+            <swiper-slide>
+                <img src="@/assets/images/lunbo2.jpg" alt="">
+            </swiper-slide>
+            <!-- Optional controls -->
+            <div class="swiper-pagination"  slot="pagination"></div>
+            <!--<div class="swiper-button-prev" slot="button-prev"></div>-->
+            <!--<div class="swiper-button-next" slot="button-next"></div>-->
+            <!--<div class="swiper-scrollbar"   slot="scrollbar"></div>-->
+        </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+
+  name: 'HelloSwiper',
+  data() {
+    return {
+      swiperOption: {
+        pagination:'.swiper-pagination'
+      }
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.imagesBox{
+    overflow: hidden;
+    width: 100%;
+    height: 0;
+    padding-bottom: 26.67%;
+}
+</style>
